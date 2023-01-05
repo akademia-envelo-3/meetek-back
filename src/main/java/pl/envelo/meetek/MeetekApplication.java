005177d113399b172c3d2249f04940a951a13355
Michał Zaborowski <zaborowskim96@gmail.com>
2023-01-05 13:33:10 +0100

    feat: BT000 - config: project set up
    Added pom.xml dependencies, gitignore and project structure

---
+++ src/main/java/pl/envelo/meetek/MeetekApplication.java
@@ -0,0 +1,13 @@
+package pl.envelo.meetek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetekApplication.class, args);
+	}
+
+}
