a1e21eca0099aa5fcdc902a02e0b479228447133
michal.zaborowski <zaborowskim96@gmail.com>
2023-01-11 14:44:38 +0100

    BT005 - feat: create controllers
    Task completed

---
+++ src/main/java/pl/envelo/meetek/controller/request/RequestBoxController.java
@@ -0,0 +1,9 @@
+package pl.envelo.meetek.controller.request;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class RequestBoxController {
+}
