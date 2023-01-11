0d1e554151299e0324c0c7db6a2caa13bd75c46f
michal.zaborowski <zaborowskim96@gmail.com>
2023-01-11 14:44:56 +0100

    BT005 - feat: create controllers
    Task completed

---
+++ src/main/java/pl/envelo/meetek/controller/event/SingleEventController.java
@@ -0,0 +1,9 @@
+package pl.envelo.meetek.controller.event;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class SingleEventController {
+}
---
+++ src/main/java/pl/envelo/meetek/controller/group/SectionController.java
@@ -0,0 +1,9 @@
+package pl.envelo.meetek.controller.group;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class SectionController {
+}
