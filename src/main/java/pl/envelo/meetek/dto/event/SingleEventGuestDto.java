064aebcd4b1fc6f70046609abd3118b379b740c8
michal.zaborowski <zaborowskim96@gmail.com>
2023-01-11 13:34:33 +0100

    BT002 - feat: create DTO classes
    Task completed

--- src/main/java/pl/envelo/meetek/dto/event/SingleEventGuestDto.java
+++ src/main/java/pl/envelo/meetek/dto/event/SingleEventGuestDto.java
@@ -1,4 +1,28 @@
 package pl.envelo.meetek.dto.event;
 
+import pl.envelo.meetek.dto.AttachmentDto;
+import pl.envelo.meetek.dto.CategoryDto;
+import pl.envelo.meetek.dto.CoordinatesDto;
+import pl.envelo.meetek.model.Hashtag;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
 public class SingleEventGuestDto {
+
+    private long eventId;
+    private Set<Hashtag> hashtags;
+    private String name;
+    private String link;
+    private String description;
+    private LocalDateTime dateTimeFrom;
+    private LocalDateTime dateTimeTo;
+    private CategoryDto category;
+    private int numberOfJoinedParticipants;
+    private String locationName;
+    private CoordinatesDto coordinates;
+    private boolean isOnline;
+    private int participantsLimit;
+    private Set<AttachmentDto> attachments;
+
 }
