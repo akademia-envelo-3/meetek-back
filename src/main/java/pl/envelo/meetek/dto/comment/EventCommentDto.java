fd7f8a90c9ec8e7a1d3679c8236e294477a8ece4
Jakub Niedzwiedzki <niedzwiedzki@adres.pl>
2023-01-12 10:53:32 +0100

    BT002 - feat: create DTO classes
    Adding missing annotation to DTO classes

--- src/main/java/pl/envelo/meetek/dto/comment/EventCommentDto.java
+++ src/main/java/pl/envelo/meetek/dto/comment/EventCommentDto.java
@@ -1,11 +1,19 @@
 package pl.envelo.meetek.dto.comment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.envelo.meetek.dto.AttachmentDto;
 import pl.envelo.meetek.dto.user.StandardUserShortDto;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventCommentDto {
 
     private long commentId;
