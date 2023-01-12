9e077de13066eca8b545fcd31152adc9fc3d5904
Michał Zaborowski <zaborowskim96@gmail.com>
2023-01-12 10:07:49 +0100

    BT094 - feat: add profiles and update readme
    Task finished

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # meetek-back
-elo1elo2
-mi teź się udało
+#### Meetek app backend project
+
+### API DOCUMENTATION
+API documentation avaible at: http://localhost:8080/swagger-ui/index.html
+
+## Database
+Runtime database is avaible at: http://localhost:8080/h2-console
+- JDBC URL: jdbc:h2:mem:test
+- User Name: sa
+- Password: 
--- src/main/java/pl/envelo/meetek/config/SwaggerConfig.java
+++ src/main/java/pl/envelo/meetek/config/SwaggerConfig.java
@@ -12,7 +12,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Meetek API", version = "1.0", description = "Meetek swagger init"))
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -22,14 +21,4 @@ public class SwaggerConfig {
                 .build();
     }
 
-//    private ApiInfo metaData() {
-//        return new ApiInfoBuilder()
-//                .title("Meetek")
-//                .description("Event planner application")
-//                .version("1.0.0")
-//                .license("Apache 2.0")
-//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-//                .contact(new Contact("Akademia Envelo", "none", "none"))
-//                .build();
-//    }
 }
