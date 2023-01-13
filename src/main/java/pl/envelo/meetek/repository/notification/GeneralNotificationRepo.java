65d4e9a741c3e3fa9febc561ad510d53daf5cee7
maciej.szczepanczyk <mckszczep@gmail.com>
2023-01-13 16:09:23 +0100

    BT006 - feat: get events - repositories
    Created methods in SingleEventRepo, modified file application-dev.yml, changed for JpaRepository in all repositories.

--- src/main/java/pl/envelo/meetek/repository/notification/GeneralNotificationRepo.java
+++ src/main/java/pl/envelo/meetek/repository/notification/GeneralNotificationRepo.java
@@ -1,9 +1,9 @@
 package pl.envelo.meetek.repository.notification;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.envelo.meetek.model.notification.GeneralNotification;
 
 @Repository
-public interface GeneralNotificationRepo extends CrudRepository<GeneralNotification, Long> {
+public interface GeneralNotificationRepo extends JpaRepository<GeneralNotification, Long> {
 }
