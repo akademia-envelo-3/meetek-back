7db83ba8aca224c3749e712be78b54be5990caed
Luke <lewickilukasz93@gmail.com>
2023-01-16 10:27:19 +0100

    BT023 - feat: delete event
    Adding deleteEvent method to controller

--- src/main/java/pl/envelo/meetek/controller/event/SingleEventController.java
+++ src/main/java/pl/envelo/meetek/controller/event/SingleEventController.java
@@ -1,11 +1,47 @@
 package pl.envelo.meetek.controller.event;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.envelo.meetek.model.event.SingleEvent;
+import pl.envelo.meetek.repository.event.SingleEventRepo;
+import pl.envelo.meetek.service.event.SingleEventService;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
 public class SingleEventController {
+
+
+    private final SingleEventService singleEventService;
+    private final SingleEventRepo singleEventRepo;
+
+
+    @GetMapping("/event/{eventId}")
+    public Optional<SingleEvent> getSingleEvent(@PathVariable long eventId){
+        return singleEventService.getOneEventById(eventId);
+
+
+    }
+
+
+    public ResponseEntity<SingleEvent> deleteEvent(@PathVariable long eventId){
+
+        if(singleEventService.getOneEventById(eventId).isPresent()){
+
+            singleEventService.deleteById(eventId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+
+    }
+
+
+
 }
