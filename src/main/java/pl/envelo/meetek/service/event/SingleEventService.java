a18951308b8f0443286183fd1b899e9e916c596a
Luke <lewickilukasz93@gmail.com>
2023-01-16 23:19:10 +0100

    BT007/8 - feat: get events - admin, past/future, all
    Added methods in SingleEventService and StandardUserController to show all past and future events for Admins.

--- src/main/java/pl/envelo/meetek/service/event/SingleEventService.java
+++ src/main/java/pl/envelo/meetek/service/event/SingleEventService.java
@@ -2,9 +2,12 @@ package pl.envelo.meetek.service.event;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.dto.event.SingleEventShortDto;
 import pl.envelo.meetek.model.event.SingleEvent;
 import pl.envelo.meetek.repository.event.SingleEventRepo;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -13,6 +16,7 @@ public class SingleEventService {
 
     private final SingleEventRepo singleEventRepo;
 
+
      public Optional<SingleEvent> getSingleEventById(long id){
          return singleEventRepo.findById(id);
      }
@@ -20,4 +24,16 @@ public class SingleEventService {
     public void deleteById(long eventId){
         singleEventRepo.deleteById(eventId);
     }
+
+    public List<SingleEvent> getAllEventsBeforeToday(){
+
+    return singleEventRepo.findAllByDateTimeFromBeforeOrderByDateTimeFromDesc(LocalDateTime.now());
+    }
+
+    public List<SingleEvent> getAllEventsAfterToday(){
+
+         return singleEventRepo.findAllByDateTimeFromAfterOrderByDateTimeFromAsc(LocalDateTime.now());
+
+    }
+
 }
