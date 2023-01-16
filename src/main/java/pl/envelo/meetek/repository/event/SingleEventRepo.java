e25d2428027b720737936cd1b5ef72e62705fcc4
maciej.szczepanczyk <mckszczep@gmail.com>
2023-01-16 10:02:49 +0100

    BT006 - feat: get events - repositories
    Created methods in SingleEventRepo, modified file application-dev.yml, changed for JpaRepository in all repositories.

--- src/main/java/pl/envelo/meetek/repository/event/SingleEventRepo.java
+++ src/main/java/pl/envelo/meetek/repository/event/SingleEventRepo.java
@@ -15,30 +15,36 @@ public interface SingleEventRepo extends JpaRepository<SingleEvent, Long> {
 
     List<SingleEvent> findAllByDateTimeFromAfterOrderByDateTimeFromAsc(LocalDateTime dateTime);
 
-    @Query(value = "SELECT * FROM single_event " +
-            "WHERE is_private IS FALSE " +
-            "AND date_time_from < ?1 " +
-            "AND single_event.event_id " +
-            "NOT IN (SELECT single_event_event_id FROM single_event_participants WHERE participants_key = ?2 AND participants_event_response_id LIKE '1') " +
-            "ORDER BY date_time_from DESC",
+    @Query(value = """
+            SELECT * FROM single_event 
+            WHERE is_private IS FALSE 
+            AND date_time_from < ?1 
+            AND single_event.event_id 
+            NOT IN (SELECT single_event_event_id FROM single_event_participants WHERE participants_key = ?2 AND participants_event_response_id LIKE '1') 
+            ORDER BY date_time_from DESC
+            """,
             nativeQuery = true)
     List<SingleEvent> findAllPublicPastNotAcceptedByUser(LocalDateTime currentDateTime, long userId);
 
-    @Query(value = "SELECT * FROM single_event " +
-            "WHERE is_private IS FALSE " +
-            "AND date_time_from > ?1 " +
-            "AND single_event.event_id " +
-            "NOT IN (SELECT single_event_event_id FROM single_event_participants WHERE participants_key = ?2 AND participants_event_response_id LIKE '1') " +
-            "ORDER BY date_time_from ASC",
+    @Query(value = """
+            SELECT * FROM single_event 
+            WHERE is_private IS FALSE 
+            AND date_time_from > ?1 
+            AND single_event.event_id 
+            NOT IN (SELECT single_event_event_id FROM single_event_participants WHERE participants_key = ?2 AND participants_event_response_id LIKE '1') 
+            ORDER BY date_time_from ASC
+            """,
             nativeQuery = true)
     List<SingleEvent> findAllPublicFutureNotAcceptedByUserAll(LocalDateTime currentDateTime, long userId);
 
-    @Query(value = "SELECT * FROM single_event " +
-            "WHERE is_private IS FALSE " +
-            "AND date_time_from BETWEEN ?1 AND ?2 " +
-            "AND single_event.event_id " +
-            "NOT IN (SELECT single_event_event_id FROM single_event_participants WHERE participants_key = ?3 AND participants_event_response_id LIKE '1') " +
-            "ORDER BY date_time_from ASC ",
+    @Query(value = """
+            SELECT * FROM single_event 
+            WHERE is_private IS FALSE 
+            AND date_time_from BETWEEN ?1 AND ?2 
+            AND single_event.event_id 
+            NOT IN (SELECT single_event_event_id FROM single_event_participants WHERE participants_key = ?3 AND participants_event_response_id LIKE '1') 
+            ORDER BY date_time_from ASC 
+            """,
             nativeQuery = true)
     List<SingleEvent> findAllPublicFutureNotAcceptedByUserForFewDays(LocalDateTime dateTimeFrom, LocalDateTime dateTimeTo, long userId);
 
