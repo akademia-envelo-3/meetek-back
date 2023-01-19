892a9de2cc56a4178a88b82fc03dde9f54063fd9
Luke <lewickilukasz93@gmail.com>
2023-01-19 16:09:27 +0100

    BT039 - feat: CRUD attachment
    Adding CRUD methods related to attachments into AttachmentService

--- src/main/java/pl/envelo/meetek/service/AttachmentService.java
+++ src/main/java/pl/envelo/meetek/service/AttachmentService.java
@@ -2,11 +2,24 @@ package pl.envelo.meetek.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.model.Attachment;
 import pl.envelo.meetek.repository.AttachmentRepo;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class AttachmentService {
 
     private final AttachmentRepo attachmentRepo;
+
+    public Attachment saveAttachment(Attachment attachment){
+        return attachmentRepo.save(attachment);
+    }
+    public Optional<Attachment> getAttachmentById(long attachmentId){
+        return attachmentRepo.findById(attachmentId);
+    }
+    public void deleteAttachmentById(long attachnentId){
+        attachmentRepo.deleteById(attachnentId);
+    }
 }
