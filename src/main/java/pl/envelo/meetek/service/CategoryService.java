3ea06c970836125b61bc369df8aaab9d6d31dc39
mnk7 <mnk7.github@gmail.com>
2023-01-19 11:39:18 +0100

    BT073 - feat: get all categories
    Adding method getAllCategories() in CategoryController and CategoryService.

--- src/main/java/pl/envelo/meetek/service/CategoryService.java
+++ src/main/java/pl/envelo/meetek/service/CategoryService.java
@@ -2,11 +2,19 @@ package pl.envelo.meetek.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.model.Category;
 import pl.envelo.meetek.repository.CategoryRepo;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class CategoryService {
 
     final private CategoryRepo categoryRepo;
+
+    public List<Category> getAllCategories() {
+        return categoryRepo.findAll();
+    }
+
 }
