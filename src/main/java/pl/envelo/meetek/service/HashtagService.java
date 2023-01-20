c1d29c36f875d0db17b125056593445b44638c9d
Jakub Niedzwiedzki <niedzwiedzki@adres.pl>
2023-01-20 12:09:38 +0100

    BT076 - feat: get all hashtags
    Added method to AdminController, HashtagService, HashtagRepo

--- src/main/java/pl/envelo/meetek/service/HashtagService.java
+++ src/main/java/pl/envelo/meetek/service/HashtagService.java
@@ -2,11 +2,18 @@ package pl.envelo.meetek.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.model.Hashtag;
 import pl.envelo.meetek.repository.HashtagRepo;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class HashtagService {
 
     private final HashtagRepo hashtagRepo;
+
+    public List<Hashtag> getAllHashtags() {
+        return hashtagRepo.findByOrderByNameAsc();
+    }
 }
