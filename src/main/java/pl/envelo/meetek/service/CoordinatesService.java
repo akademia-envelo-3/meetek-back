3d75bbb098ea28c110d6b7a49e9bb92b5331baa2
Luke <lewickilukasz93@gmail.com>
2023-01-20 11:03:23 +0100

    BT041 - feat: CRUD coordinates
    Addint CRUD methods to CoordinatesService

--- src/main/java/pl/envelo/meetek/service/CoordinatesService.java
+++ src/main/java/pl/envelo/meetek/service/CoordinatesService.java
@@ -2,11 +2,22 @@ package pl.envelo.meetek.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.model.Coordinates;
 import pl.envelo.meetek.repository.CoordinatesRepo;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class CoordinatesService {
 
     private final CoordinatesRepo coordinatesRepo;
+
+    public Coordinates createCoordinates(Coordinates coordinates) {
+        return coordinatesRepo.save(coordinates);
+    }
+
+    public Optional<Coordinates> getCoordinatesById(long coordinatesId) {
+        return coordinatesRepo.findById(coordinatesId);
+    }
 }
