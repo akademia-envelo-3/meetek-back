01ee332d7571fb58a18b3c3f8bf96a39c2f5fc89
maciej.szczepanczyk <mckszczep@gmail.com>
2023-01-23 10:09:39 +0100

    BT087/BT091_-_CRUD_general_notification/group_notification
    added methods to create and findById in GroupNotificationService & GeneralNotificationService

--- src/main/java/pl/envelo/meetek/service/notification/GeneralNotificationService.java
+++ src/main/java/pl/envelo/meetek/service/notification/GeneralNotificationService.java
@@ -2,11 +2,23 @@ package pl.envelo.meetek.service.notification;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.model.notification.GeneralNotification;
 import pl.envelo.meetek.repository.notification.GeneralNotificationRepo;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class GeneralNotificationService {
 
     private final GeneralNotificationRepo generalNotificationRepo;
+
+    public GeneralNotification createNotification(GeneralNotification generalNotification) {
+        return generalNotificationRepo.save(generalNotification);
+    }
+
+    public Optional<GeneralNotification> getById(long notificationId) {
+        return generalNotificationRepo.findById(notificationId);
+    }
+
 }
