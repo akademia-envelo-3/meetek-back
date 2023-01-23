44c947d7bf3378489c5244f959d9795f8a44b87f
Luke <lewickilukasz93@gmail.com>
2023-01-23 11:44:32 +0100

    BT089 - feat: CRUD comment notification
    Adding CRUD methods to CommentNotificationService

--- src/main/java/pl/envelo/meetek/service/notification/CommentNotificationService.java
+++ src/main/java/pl/envelo/meetek/service/notification/CommentNotificationService.java
@@ -2,11 +2,22 @@ package pl.envelo.meetek.service.notification;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.meetek.model.notification.CommentNotification;
 import pl.envelo.meetek.repository.notification.CommentNotificationRepo;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class CommentNotificationService {
 
     private final CommentNotificationRepo commentNotificationRepo;
+
+    public Optional<CommentNotification> getCommentNotificationById(long commentNotificationId) {
+        return commentNotificationRepo.findById(commentNotificationId);
+    }
+
+    public void createCommentNotification(CommentNotification commentNotification) {
+        commentNotificationRepo.save(commentNotification);
+    }
 }
