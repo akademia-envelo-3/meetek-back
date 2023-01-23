e6f2a79f0f0d32d6229a561d2da1f904e1d50403
maciej.szczepanczyk <mckszczep@gmail.com>
2023-01-23 14:44:05 +0100

    BT067 - feat: create recurring event set
    added methods to create in RecurringEventSet

--- src/main/java/pl/envelo/meetek/model/event/RecurringEventSet.java
+++ src/main/java/pl/envelo/meetek/model/event/RecurringEventSet.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 import java.util.Set;
@@ -11,6 +12,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
 public class RecurringEventSet {
 
