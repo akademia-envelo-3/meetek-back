bc19dbcdcb5f8721f22913db258587ca0521b385
michal.zaborowski <zaborowskim96@gmail.com>
2023-02-08 08:31:44 +0100

    feat: swagger groups updated

--- src/main/java/pl/envelo/meetek/config/SwaggerConfig.java
+++ src/main/java/pl/envelo/meetek/config/SwaggerConfig.java
@@ -12,55 +12,55 @@ public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi allOpenApi() {
-        String packages[] = {"pl.envelo.meetek"};
+        String[] packages = {"pl.envelo.meetek"};
         return GroupedOpenApi.builder().group("all").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi eventsOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.event"};
+        String[] packages = {"pl.envelo.meetek.domain.event"};
         return GroupedOpenApi.builder().group("events").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi sectionsOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.group"};
+        String[] packages = {"pl.envelo.meetek.domain.group"};
         return GroupedOpenApi.builder().group("sections").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi notificationsOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.notification"};
+        String[] packages = {"pl.envelo.meetek.domain.notification"};
         return GroupedOpenApi.builder().group("notifications").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi requestOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.request"};
+        String[] packages = {"pl.envelo.meetek.domain.request"};
         return GroupedOpenApi.builder().group("requests").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi surveyOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.survey"};
+        String[] packages = {"pl.envelo.meetek.domain.survey"};
         return GroupedOpenApi.builder().group("surveys").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi userOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.user"};
+        String[] packages = {"pl.envelo.meetek.domain.user"};
         return GroupedOpenApi.builder().group("users").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi categoryOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.category"};
+        String[] packages = {"pl.envelo.meetek.domain.category"};
         return GroupedOpenApi.builder().group("categories").packagesToScan(packages).build();
     }
 
     @Bean
     public GroupedOpenApi hashtagOpenApi() {
-        String packages[] = {"pl.envelo.meetek.controller.hashtag"};
+        String[] packages = {"pl.envelo.meetek.domain.hashtag"};
         return GroupedOpenApi.builder().group("hashtags").packagesToScan(packages).build();
     }
 
