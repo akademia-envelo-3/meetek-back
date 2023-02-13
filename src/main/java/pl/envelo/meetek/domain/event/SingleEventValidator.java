4e77f7762846be6c664c304afd035e977e2a9b87
maciej.szczepanczyk <mckszczep@gmail.com>
2023-02-13 15:29:45 +0100

    BT022 + BT054
    added updateOwner validator

--- src/main/java/pl/envelo/meetek/domain/event/SingleEventValidator.java
+++ src/main/java/pl/envelo/meetek/domain/event/SingleEventValidator.java
@@ -3,6 +3,9 @@ package pl.envelo.meetek.domain.event;
 import jakarta.validation.Validator;
 import org.springframework.stereotype.Service;
 import pl.envelo.meetek.domain.event.model.SingleEvent;
+import pl.envelo.meetek.domain.user.StandardUserValidator;
+import pl.envelo.meetek.domain.user.model.StandardUser;
+import pl.envelo.meetek.exceptions.DuplicateException;
 import pl.envelo.meetek.exceptions.NotFoundException;
 import pl.envelo.meetek.utils.ValidatorService;
 
@@ -13,9 +16,12 @@ public class SingleEventValidator extends ValidatorService<SingleEvent> {
 
     private final SingleEventRepo singleEventRepo;
 
-    public SingleEventValidator(Validator validator, SingleEventRepo singleEventRepo) {
+    private final StandardUserValidator userValidator;
+
+    public SingleEventValidator(Validator validator, SingleEventRepo singleEventRepo,StandardUserValidator userValidator) {
         super(validator);
         this.singleEventRepo = singleEventRepo;
+        this.userValidator = userValidator;
     }
 
     @Override
@@ -35,6 +41,13 @@ public class SingleEventValidator extends ValidatorService<SingleEvent> {
         } else {
             return days;
         }
+    }
+
+    public StandardUser validateOwnerForAdmin(SingleEvent event, long newOwnerId){
+        if(event.getOwner().getParticipantId().equals(newOwnerId)){
+            throw new DuplicateException("User with id " + newOwnerId + " is already the owner of this event");
+        }
+        return userValidator.validateExists(newOwnerId);
     }
 
 }
