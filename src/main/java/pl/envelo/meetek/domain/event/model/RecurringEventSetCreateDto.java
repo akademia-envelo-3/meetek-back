a83fa5c169300337e41b6a1fe4fb1d78a14ea609
maciej.szczepanczyk <mckszczep@gmail.com>
2023-02-22 14:35:32 +0100

    BT068/BT065 added data from singleEvent to RecurringEventSetCreateDto

--- src/main/java/pl/envelo/meetek/domain/event/model/RecurringEventSetCreateDto.java
+++ src/main/java/pl/envelo/meetek/domain/event/model/RecurringEventSetCreateDto.java
@@ -4,6 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.envelo.meetek.domain.attachment.AttachmentDto;
+import pl.envelo.meetek.domain.category.CategoryDto;
+import pl.envelo.meetek.domain.coordinates.CoordinatesDto;
+import pl.envelo.meetek.domain.hashtag.HashtagDto;
+import pl.envelo.meetek.domain.survey.model.SurveyCreateDto;
+import pl.envelo.meetek.domain.user.model.StandardUserDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -13,5 +23,24 @@ public class RecurringEventSetCreateDto {
 
     private int eventFrequency;
     private int recursiveCount;
+
+    private Set<HashtagDto> hashtags;
+    private StandardUserDto owner;
+    private String name;
+    private String link;
+    private String description;
+    private LocalDateTime dateTimeFrom;
+    private LocalDateTime dateTimeTo;
+    private CategoryDto category;
+    private Set<StandardUserDto> invitedUsers;
+    private String locationName;
+    private CoordinatesDto coordinates;
+    private Boolean isOnline;
+    private Boolean isExternal;
+    private Boolean isPrivate;
+    private Boolean isResponseRequired;
+    private Integer participantsLimit;
+    private List<AttachmentDto> attachments;
+    private Set<SurveyCreateDto> surveys;
 
 }
