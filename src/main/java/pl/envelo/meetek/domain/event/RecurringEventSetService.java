0b343ce1cde65941324286f6e18291d32afb7297
maciej.szczepanczyk <mckszczep@gmail.com>
2023-02-22 16:29:07 +0100

    BT068/BT065 added createRecurringEventSet

--- src/main/java/pl/envelo/meetek/domain/event/RecurringEventSetService.java
+++ src/main/java/pl/envelo/meetek/domain/event/RecurringEventSetService.java
@@ -3,10 +3,12 @@ package pl.envelo.meetek.domain.event;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.envelo.meetek.domain.event.model.RecurringEventSet;
-import pl.envelo.meetek.domain.event.model.RecurringEventSetCreateDto;
-import pl.envelo.meetek.domain.event.model.RecurringEventSetDto;
+import pl.envelo.meetek.domain.event.model.*;
+import pl.envelo.meetek.domain.user.model.StandardUser;
 import pl.envelo.meetek.utils.DtoMapperService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
@@ -15,13 +17,26 @@ public class RecurringEventSetService {
     private final RecurringEventSetRepo recurringEventSetRepo;
     private final RecurringEventSetValidator recurringEventSetValidator;
     private final DtoMapperService mapperService;
+    private final SingleEventService eventService;
+
 
     @Transactional
-    public RecurringEventSetDto createRecurringEventSet(RecurringEventSetCreateDto recurringEventSetDto) {
-        RecurringEventSet recurringEventSet = mapperService.mapToRecurringEventSet(recurringEventSetDto);
-        recurringEventSetValidator.validateInput(recurringEventSet);
-        recurringEventSet = recurringEventSetRepo.save(recurringEventSet);
-        return mapperService.mapToRecurringEventSetDto(recurringEventSet);
+    public RecurringEventSet createRecurringEventSet( StandardUser owner, RecurringEventSetCreateDto eventSetCreateDto, SingleEvent event){
+        RecurringEventSet eventSet = mapperService.mapToRecurringEventSet(eventSetCreateDto);
+        recurringEventSetValidator.validateInput(eventSet);
+        eventSet = recurringEventSetRepo.save(eventSet);
+        SingleEventCreateDto eventCreateDto = mapperService.mapToSingleEventCreateDto(event);
+        Set<SingleEvent> singleEvents = new HashSet<>();
+
+        for(int i = 0; i < eventSet.getRecursiveCount(); i++) {
+            eventCreateDto.setDateTimeFrom(eventCreateDto.getDateTimeFrom().plusDays(eventSet.getEventFrequency()));
+            eventCreateDto.setDateTimeTo(eventCreateDto.getDateTimeTo().plusDays(eventSet.getEventFrequency()));
+
+            SingleEvent singleEvent = mapperService.mapToSingleEvent(eventService.createEvent(owner,eventCreateDto));
+            singleEvents.add(singleEvent);
+        }
+        eventSet.setEvents(singleEvents);
+        return eventSet;
     }
 
 }
